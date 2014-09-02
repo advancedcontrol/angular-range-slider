b6e5d696e55c833fdb2d8fba0e64cc56c8d58fce
Will Cannings <me@willcannings.com>
2014-09-02 14:51:46 +1000

    Initial version of the range directive

--- angular-range-slider.js
+++ angular-range-slider.js
@@ -1,14 +1,27 @@
 (function (angular) {
     'use strict';
 
-    angular.module('coRangeSlider', ['ngGesture', 'coAnimate', 'SafeApply'])
+    angular.module('coRangeSlider', ['ngGesture', 'coAnimate'])
         .directive('range', [
-            '$window',
-            '$timeout',
-            '$safeApply',
             '$animation',
-            function ($window, $timeout, $safeApply, $animation) {
 
+            function ($animation) {
+                return {
+                    restrict: 'E',
+                    replace: true,
+                    scope: {
+                        options: '=?',
+                        model: '='
+                    },
+
+                    template:   '<div class="co-range-slider" ng-class="{disabled: options.disabled, readonly: options.readonly, vert: options.vertical, horz: options.horizontal, animate: animate}"' + 
+                                'ng-click="clicked($event)" touch-action="pan-y" drag-begin="dragStart($event, true)" drag-stop="dragEnd()" ng-drag="drag($position)">' +
+                                    '<div class="track"></div>' +
+                                    '<div class="progress"></div>' +
+                                    '<span class="handle" role="slider" touch-action="pan-y" drag-begin="dragStart($event)" drag-stop="dragEnd()" ng-drag="drag($position)"></span>' +
+                                    '<input type="number" data-type="range" ng-class="{hide: !options.visible}" max="{{options.max}}" min="{{options.min}}" step="{{options.step}}" />' +
+                                '</div>'
+                }
             }
         ]);
 
